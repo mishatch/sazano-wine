c4f5eeed87ce59838271b223fe9cc4aca4333e3e
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-09-23 02:03:23 +0400

    fix modal issue

--- script/dialog.js
+++ script/dialog.js
@@ -1,17 +1,37 @@
 // Open and Close Dialog
-
-const twinModal = document.querySelector(".modal");
-const openModalstTwin = document.querySelector(".twin-btn");
-const closeModal = document.querySelector(".close-icon");
-
-openModalstTwin.addEventListener("click", () => {
-  twinModal.showModal();
-  twinModal.scrollTop = 0;
-});
-closeModal.addEventListener("click", () => {
-  twinModal.close();
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
 });
 
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  } else {
+    modal.classList.add("active");
+    overlay.classList.add("active");
+  }
+}
+function closeModal(modal) {
+  if (modal == null) {
+    return;
+  } else {
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+}
 // Carousel Animation
 const buttons = document.querySelectorAll("[data-carousel-button]");
 buttons.forEach((button) => {
