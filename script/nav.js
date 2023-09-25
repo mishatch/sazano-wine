ba5b4684ad2a1e5bfaadb9b4a4f3f4b043752b78
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-09-25 22:17:35 +0400

    add menu

--- script/nav.js
+++ script/nav.js
@@ -1,31 +1,31 @@
 //Sandwich navigation bar animation
 
-const button = document.querySelector(".nav-btn");
+const navBtn = document.querySelector(".nav-btn");
 const nav = document.querySelector(".nav");
 const links = document.querySelector(".links");
 let clicked = false;
 
-button.addEventListener("click", () => {
+navBtn.addEventListener("click", () => {
   console.log("Button was clicked!");
   if (!clicked) {
     nav.style.backgroundColor = "#4c0027";
     links.style.display = "flex";
-    button.style.width = "auto";
-    button.style.height = "30px";
-    button.style.paddingTop = "0";
-    button.style.marginTop = "0";
-    button.style.backgroundColor = "transparent";
+    navBtn.style.width = "auto";
+    navBtn.style.height = "30px";
+    navBtn.style.paddingTop = "0";
+    navBtn.style.marginTop = "0";
+    navBtn.style.backgroundColor = "transparent";
     links.classList.remove("hidden");
     clicked = true;
   } else {
     nav.style.backgroundColor = "transparent";
     links.classList.add("hidden");
     links.style.display = "none";
-    button.style.width = "70px";
-    button.style.height = "70px";
-    button.style.paddingTop = "33px";
-    button.style.marginTop = "-35px";
-    button.style.backgroundColor = "#4c0027";
+    navBtn.style.width = "70px";
+    navBtn.style.height = "70px";
+    navBtn.style.paddingTop = "33px";
+    navBtn.style.marginTop = "-35px";
+    navBtn.style.backgroundColor = "#4c0027";
     clicked = false;
   }
 });
@@ -33,11 +33,64 @@ function closeNav() {
   nav.style.backgroundColor = "transparent";
   links.classList.add("hidden");
   links.style.display = "none";
-  button.style.width = "70px";
-  button.style.height = "70px";
-  button.style.paddingTop = "33px";
-  button.style.marginTop = "-35px";
-  button.style.backgroundColor = "#4c0027";
+  navBtn.style.width = "70px";
+  navBtn.style.height = "70px";
+  navBtn.style.paddingTop = "33px";
+  navBtn.style.marginTop = "-35px";
+  navBtn.style.backgroundColor = "#4c0027";
   clicked = false;
 }
-///
+
+//Open and close menu modal.
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+    if (clicked) {
+      nav.style.backgroundColor = "transparent";
+      links.classList.add("hidden");
+      links.style.display = "none";
+      navBtn.style.width = "70px";
+      navBtn.style.height = "70px";
+      navBtn.style.paddingTop = "33px";
+      navBtn.style.marginTop = "-35px";
+      navBtn.style.backgroundColor = "#4c0027";
+      clicked = false;
+    }
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  } else {
+    modal.classList.add("active");
+    overlay.classList.add("active");
+  }
+}
+function closeModal(modal) {
+  if (modal == null) {
+    return;
+  } else {
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+}
